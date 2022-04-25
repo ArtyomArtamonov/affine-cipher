2f5752e90c44b56b528c267bb5863b134e3d83a8
Artyom Artamonov <artem02artamonov@gmail.com>
2022-04-25 19:38:37 +0300

    changes: - struct instead of raw functions - unit test with cases written

--- cmd/main.go
+++ cmd/main.go
@@ -10,68 +10,77 @@ import (
 
 const dictionary = "abcdefghijklmnopqrstuvwxyz "
 
-// These two maps are only for optimization purposes 
-var runeWithIndex map[rune]int
-var indexWithRune map[int]rune
-
-func initialize() {
-	runeWithIndex = make(map[rune]int)
-	indexWithRune = make(map[int]rune)
-
-	for index, ch := range dictionary {
-		runeWithIndex[ch] = index
-		indexWithRune[index] = ch
-	}
-}
-
 func main() {
 	// Input data
-	str := "hello world"
+	str := "serega krasava"
 	// Keys
 	a, b := 4, 4
 
 	// Initialize populates two maps with dictionary data combined with indexes
-	initialize()
+	st := NewAffineCipherManager(dictionary)
 
 	fmt.Println("Initial text: " + str)
 	fmt.Println("---------------------------------")
 
-	encrypted := Encrypt(str, a, b)
+	encrypted := st.Encrypt(str, a, b)
 	fmt.Printf("Encrypted: %s\n", encrypted)
 	fmt.Println("---------------------------------")
 
-	fmt.Printf("Decrypted: %s\n", Decrypt(encrypted, a, b))
+	fmt.Printf("Decrypted: %s\n", st.Decrypt(encrypted, a, b))
+}
+
+type AffineCipherManager struct {
+	dictionary string
+
+	// These two maps are only for optimization purposes 
+	runeWithIndex map[rune]int
+	indexWithRune map[int]rune
+}
+
+func NewAffineCipherManager(dictionary string) *AffineCipherManager {
+	st := &AffineCipherManager{
+		dictionary:    dictionary,
+		runeWithIndex: make(map[rune]int),
+		indexWithRune: make(map[int]rune),
+	}
+
+	for index, ch := range st.dictionary {
+		st.runeWithIndex[ch] = index
+		st.indexWithRune[index] = ch
+	}
+
+	return st
 }
 
 // Encrypt given text with given keys a and b
-func Encrypt(text string, a, b int) string {
-	if !coprime(a, len(dictionary)) {
+func (m *AffineCipherManager) Encrypt(text string, a, b int) string {
+	if !coprime(a, len(m.dictionary)) {
 		log.Fatal("Key A and dictionary length should be Co-Prime numbers!")
 	}
 
-	dictionaryLength := len(dictionary)
+	dictionaryLength := len(m.dictionary)
 	result := ""
 
 	for _, ch := range text {
-		nextIndex := (a*runeWithIndex[ch] + b) % dictionaryLength
+		nextIndex := (a*m.runeWithIndex[ch] + b) % dictionaryLength
 		// Optimization map indexWithRune helps to remain O(1) when finding letter by index
-		nextLetter := indexWithRune[nextIndex]
+		nextLetter := m.indexWithRune[nextIndex]
 		result += string(nextLetter)
 	}
 
 	return result
 }
 
-func Decrypt(encrypted string, a, b int) string {
-	dictionaryLength := len(dictionary)
+func (m *AffineCipherManager) Decrypt(encrypted string, a, b int) string {
+	dictionaryLength := len(m.dictionary)
 	inverse := 0
 	result := ""
 	// Trying to count inverse value (a^-1)
 	for ; inverse * a % dictionaryLength != 1; inverse++ {}
 	
 	for _, ch := range encrypted {
-		nextIndex := inverse * (runeWithIndex[ch] + dictionaryLength - b) % dictionaryLength
-		nextLetter := indexWithRune[nextIndex]
+		nextIndex := inverse * (m.runeWithIndex[ch] + dictionaryLength - b) % dictionaryLength
+		nextLetter := m.indexWithRune[nextIndex]
 		result += string(nextLetter)
 	}
 
--- cmd/main_test.go
+++ cmd/main_test.go
@@ -3,12 +3,40 @@ package main
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func BenchmarkEncrypt(b *testing.B) {
 	var a, c = 4, 4
+	var dictionary = "abcdefghijklmnopqrstuvwxyz "
+	st := NewAffineCipherManager(dictionary)
 	for i := 0; i < b.N; i++ {
-		Encrypt(fmt.Sprintf("Test text %d", i), a, c)
+		st.Encrypt(fmt.Sprintf("Test text %d", i), a, c)
+	}
+}
+
+func TestDecrypt(t *testing.T) {
+	var a, b = 4, 4
+	var dictionary = "abcdefghijklmnopqrstuvwxyz "
+	var cases = []struct{
+		encrypted string
+		decrypted string
+	}{
+		{
+			encrypted: "fuvvgalgsvq",
+			decrypted: "hello world",
+		},
+		{
+			encrypted: "wusubearsewehe",
+			decrypted: "serega krasava",
+		},
+	}
+
+	st := NewAffineCipherManager(dictionary)
+	for _, c := range cases {
+		decrypted := st.Decrypt(c.encrypted, a, b)
+		assert.Equal(t, c.decrypted, decrypted)
 	}
 }
 
